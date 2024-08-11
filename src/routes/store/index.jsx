511f64d68a64e03215c1bb35c0179298b576411c
MeDeveloperWeb <www.shivachaurasia@gmail.com>
2024-08-11 21:26:37 +0530

    fix: "Fixed Loading svg"

--- src/routes/store/index.jsx
+++ src/routes/store/index.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import Header from './_components/Header';
 import Product from '../_components/Product';
-import loadingSvg from '../../assets/icons/loading.svg?react';
 import { getProducts } from '../_lib/storeUtil';
+import LoadingSvg from '../../assets/icons/loading.svg?react';
 
 export default function Store() {
   const [filter, setFilter] = useState({
@@ -22,20 +22,24 @@ export default function Store() {
   }, [filter]);
 
   return (
-    <main className="flex-1">
+    <main className="flex flex-1 flex-col">
       <Header filter={filter} setFilter={setFilter} />
-      <div className="grid grid-cols-2 gap-4 px-2 pb-16 pt-4 md:grid-cols-3 md:gap-8 md:px-4 lg:grid-cols-4">
-        {!products
-          ? loadingSvg()
-          : products.map((product) => (
-              <Product
-                key={product.id}
-                image={product.image}
-                title={product.title}
-                id={product.id}
-              />
-            ))}
-      </div>
+      {!products ? (
+        <div className="flex h-full flex-1 items-center justify-center">
+          <LoadingSvg width={60} height={60} />
+        </div>
+      ) : (
+        <div className="grid flex-1 grid-cols-2 gap-4 px-2 pb-16 pt-4 md:grid-cols-3 md:gap-8 md:px-4 lg:grid-cols-4">
+          {products.map((product) => (
+            <Product
+              key={product.id}
+              image={product.image}
+              title={product.title}
+              id={product.id}
+            />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
